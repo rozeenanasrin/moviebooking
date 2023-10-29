6f90d6c3a3c7d466d33ffac05d0c3e40779d4647
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 20:55:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/movies') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('All Movies Data in JSON format from Mongo DB');
+  } else if (req.url === '/genres') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('All Genres Data in JSON format from Mongo DB');
+  } else if (req.url === '/artists') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end('All Artists Data in JSON format from Mongo DB');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+const port = 9000;
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
