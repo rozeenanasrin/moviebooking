ac1f35891caead46ea0b1d36851dbc2b1fd4606d
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 22:24:32 +0530

    Update genre.model.js

--- models/genre.model.js
+++ models/genre.model.js
@@ -1,1 +1,12 @@
 
+const mongoose = require("mongoose");
+
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    genreid: Number,
+    genre: String,
+  })
+);
+
+module.exports = Genre;
