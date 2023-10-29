22a761ea8294088e58a3e672df6248af3ff513ba
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 22:30:38 +0530

    Update movie.model.js

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
+
+const Movie = mongoose.model(
+  "Movie",
+  new mongoose.Schema({
+    movieid: Number,
+    title: String,
+    published: Boolean,
+    released: Boolean,
+    release_date: Number,
+    publish_date: Number
+  })
+);
+
+module.exports = Movie;
 
