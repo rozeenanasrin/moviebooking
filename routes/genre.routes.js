b60d9adb56725241b34d4b7e8586a8cf0689fc4c
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 23:56:04 +0530

    Update genre.routes.js

--- routes/genre.routes.js
+++ routes/genre.routes.js
@@ -1,10 +1,10 @@
-
-// app/routes/genre.routes.js
-const express = require("express");
-const router = express.Router();
-const genreController = require("../controllers/genre.controller");
-
-// GET /genres
-router.get("/", genreController.findAllGenres);
-
-module.exports = router;
+module.exports = app => {
+    var genres= require("../controllers/genre.controller");
+  
+    var router = require("express").Router();
+  
+    router.get('/genres',genres.findAllGenres);
+  
+    app.use('/api', router);
+  } 
+  
