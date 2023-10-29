5a83c8a969b7f95d27b47804831835263e775493
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 23:56:42 +0530

    Update movie.routes.js

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,22 +1,20 @@
 
-
-const express = require("express");
-const router = express.Router();
-const movieController = require("../controllers/movie.controller");
-
-
-router.get("/", movieController.findAllMovies);
-
-
-router.get("/published", movieController.findAllMovies);
-
-
-router.get("/released", movieController.findAllMovies);
-
-
-router.get("/:movieId", movieController.findOne);
-
-
-router.get("/search", movieController.findShows);
-
-module.exports = router;
+module.exports= app =>{
+    const movies= require("../controllers/movie.controller.js");
+  
+    var router = require("express").Router();
+  
+  
+    router.get('/movies',movies.findAllMovies);
+  
+    router.get('/movies?status=PUBLISHED',);
+  
+    router.get('/movies?status=RELEASED',);
+  
+    router.get('/movies/:movieId',movies.findOne);
+  
+   //router.get('/movies?status=RELEASED&title={title}&genres={genres}&artists={artists}&start_date={startdate}&end_date={enddate}',);
+  
+    app.use('/api', router);
+  };
+  
