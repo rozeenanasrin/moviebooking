18ab18f6c8b6159e7359c801986d209645fc4a1b
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 23:12:54 +0530

    Update artist.controller.js

--- controllers/artist.controller.js
+++ controllers/artist.controller.js
@@ -1,1 +1,11 @@
 
+const Artist = require('../models/Artist'); 
+
+exports.findAllArtists = async (req, res) => {
+  try {
+    const artists = await Artist.find(); 
+    res.json(artists);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching artists' });
+  }
+};
