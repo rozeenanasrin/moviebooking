bbe305e87b886c54f2e550d30e763d0c693b5792
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 23:14:57 +0530

    Update genre.controller.js

--- controllers/genre.controller.js
+++ controllers/genre.controller.js
@@ -1,1 +1,13 @@
 
+const Genre = require('../models/Genre'); 
+
+// Find all genres
+exports.findAllGenres = async (req, res) => {
+  try {
+    
+    const genres = await Genre.find();
+    res.json(genres);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching genres' });
+  }
+};
