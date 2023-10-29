2e6d54aa2caf6ac4fdd016c09f24ad0d04e229af
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 23:19:52 +0530

    Update artist.routes.js

--- routes/artist.routes.js
+++ routes/artist.routes.js
@@ -1,1 +1,9 @@
 
+
+const express = require("express");
+const router = express.Router();
+const artistController = require("../controllers/artist.controller");
+
+router.get("/", artistController.findAllArtists);
+
+module.exports = router;
