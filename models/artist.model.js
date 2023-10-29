3ed783d77047ae35a0bb831ac7b00de0336ae832
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 22:24:09 +0530

    Update artist.model.js

--- models/artist.model.js
+++ models/artist.model.js
@@ -1,1 +1,16 @@
 
+const mongoose = require("mongoose");
+
+const Artist = mongoose.model(
+  "Artist",
+  new mongoose.Schema({
+    artistid: Number,
+    first_name: String,
+    last_name: String,
+    wiki_url: String,
+    profile_url: String,
+    movies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
+  })
+);
+
+module.exports = Artist;
