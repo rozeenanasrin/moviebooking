67cdd0c57409519634bb4868c1f0231165103c8d
rozeenanasrin <53082598+rozeenanasrin@users.noreply.github.com>
2023-10-29 22:36:05 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,12 @@
 
+const Artist = require("./artist.model");
+const Genre = require("./genre.model");
+const Movie = require("./movie.model");
+const User = require("./user.model");
+
+module.exports = {
+  Artist,
+  Genre,
+  Movie,
+  User,
+};
